09508285c21428374ad2c71e0292a70c9805ca51
Martín Badino <151761027+MartoBadi@users.noreply.github.com>
2024-10-13 20:09:37 -0300

    Update app_testing.py

--- app_testing.py
+++ app_testing.py
@@ -2,30 +2,33 @@ import streamlit as st
 import tensorflow as tf
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
-import numpy as np
-import os
 from PIL import Image
+import numpy as np
 
 # Cargar el modelo TFLite
 interpreter = tf.lite.Interpreter(model_path='model.tflite')
 interpreter.allocate_tensors()
 
-# Obtener detalles de entrada y salida
-input_details = interpreter.get_input_details()
-output_details = interpreter.get_output_details()
-
 # Definir nombres de clases
 class_names = ['burj_khalifa', 'chichen_itza', 'christ the reedemer', 'eiffel_tower', 'great_wall_of_china', 'machu_pichu', 'pyramids_of_giza', 'roman_colosseum', 'statue_of_liberty', 'stonehenge', 'taj_mahal', 'venezuela_angel_falls']
 
-# Preprocesar la imagen subida
 def preprocess_image(image):
-    size = (150, 150)
+    # Redimensionar la imagen al tamaño esperado por el modelo
+    image = image.resize((150, 150))
+    
+    # Convertir la imagen a un array de numpy
     image = np.array(image)
-    image = np.resize(image, (size[0], size[1], 3))  # Redimensionar la imagen
-    img_array = image / 255.0  # Normalizar la imagen
-    img_array = np.expand_dims(img_array, axis=0)
-    img_array = img_array.astype(np.float32) 
-    return image_array
+    
+    # Normalizar la imagen (asegúrate de que esto coincida con el notebook)
+    image = image / 255.0
+    
+    # Expandir las dimensiones para que coincida con el formato esperado por el modelo
+    image = np.expand_dims(image, axis=0)
+    
+    # Asegurarse de que la imagen sea de tipo FLOAT32
+    image = image.astype(np.float32)
+    
+    return image
 
 st.title("Clasificación de imágenes de maravillas del mundo")
 st.write("Este sitio web fue creado para la materia Modelizado de Sistemas de IA de la Tecnicatura Superior en Ciencias de Datos e Inteligencia Artificial del IFTS 18. La idea es que subas una imagen de uno de las siguientes maravillas del mundo: burj_khalifa, chichen_itza, christ the reedemer, eiffel_tower, great_wall_of_china, machu_pichu, pyramids_of_giza, roman_colosseum, statue_of_liberty, stonehenge, taj_mahal, venezuela_angel_falls y el modelo te dirá qué maravilla aparece en la imagen. ¡Diviértete!")
@@ -50,9 +53,9 @@ if uploaded_file is not None:
     
     # Obtener la clase con mayor probabilidad
     predicted_class = class_names[np.argmax(predictions)]
-    
     # Obtener la clase real (nombre de la carpeta)
     real_class = os.path.basename(os.path.dirname(uploaded_file.name))
     
     st.write(f"Real class: {real_class}")
-    st.write(f"Predicted class: {predicted_class}")
+    st.write(f"Prediction: {predicted_class}")
+  
