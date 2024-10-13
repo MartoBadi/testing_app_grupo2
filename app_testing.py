fb597f04ceabbd76b9e5f15824c4880ab07f2b7c
Martín Badino <151761027+MartoBadi@users.noreply.github.com>
2024-10-13 19:56:03 -0300

    Corregí la función para preprocesar la imagen

--- app_testing.py
+++ app_testing.py
@@ -19,22 +19,13 @@ class_names = ['burj_khalifa', 'chichen_itza', 'christ the reedemer', 'eiffel_to
 
 # Preprocesar la imagen subida
 def preprocess_image(image):
-    # Redimensionar la imagen al tamaño esperado por el modelo
-    image = Image.fromarray(image).resize((150, 150))
-    
-    # Convertir la imagen a un array de numpy
+    size = (150, 150)
     image = np.array(image)
-    
-    # Normalizar la imagen
-    img_array = image / 255.0
-    
-    # Expandir las dimensiones para que coincida con el formato esperado por el modelo
+    image = np.resize(image, (size[0], size[1], 3))  # Redimensionar la imagen
+    img_array = image / 255.0  # Normalizar la imagen
     img_array = np.expand_dims(img_array, axis=0)
-    
-    # Asegurarse de que la imagen sea de tipo FLOAT32
-    img_array = img_array.astype(np.float32)
-    
-    return img_array
+    img_array = img_array.astype(np.float32) 
+    return image
 
 st.title("Clasificación de imágenes de maravillas del mundo")
 st.write("Este sitio web fue creado para la materia Modelizado de Sistemas de IA de la Tecnicatura Superior en Ciencias de Datos e Inteligencia Artificial del IFTS 18. La idea es que subas una imagen de uno de las siguientes maravillas del mundo: burj_khalifa, chichen_itza, christ the reedemer, eiffel_tower, great_wall_of_china, machu_pichu, pyramids_of_giza, roman_colosseum, statue_of_liberty, stonehenge, taj_mahal, venezuela_angel_falls y el modelo te dirá qué maravilla aparece en la imagen. ¡Diviértete!")
