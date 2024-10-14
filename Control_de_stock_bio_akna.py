8e75d790bb7ea93df3f702911eafbedf1c2a9cdf
Martín Badino <151761027+MartoBadi@users.noreply.github.com>
2024-10-14 15:41:37 -0300

    Actualizar Control_de_stock_bio_akna.py
    Agregué productos al diccionario

--- Control_de_stock_bio_akna.py
+++ Control_de_stock_bio_akna.py
@@ -1,3 +1,7 @@
-producto = [
-{
+# Definición de productos como diccionarios
+productos = [
+    {"nombre": "Chips de chocolate", "precio": "1.800 por 100 gramos", "categoria": "Producto a un precio por kilo", "ubicacion": "Heladera"},
+    {"nombre": "Jugo de manzana roja Pura Frutta 1 litro", "precio": "3.500 $", "ubicacion": "Heladera"},
+    {"nombre": "Jugo de sabor multifruta (manzana, pera, banana y naranja) Pura Frutta 1 litro", "precio": "3.500 $", "ubicacion": "Heladera"},
 ]
+
