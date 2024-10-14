0e15186f281c080da739f657abed85e29882c092
Martín Badino <151761027+MartoBadi@users.noreply.github.com>
2024-10-14 15:47:15 -0300

    Actualizar Control_de_stock_bio_akna.py
    Agregué una barra de búsqueda

--- Control_de_stock_bio_akna.py
+++ Control_de_stock_bio_akna.py
@@ -1,7 +1,26 @@
+import streamlit as st
+
 # Definición de productos como diccionarios
 productos = [
-    {"nombre": "Chips de chocolate", "precio": "1.800 por 100 gramos", "categoria": "Producto a un precio por kilo", "ubicacion": "Heladera"},
-    {"nombre": "Jugo de manzana roja Pura Frutta 1 litro", "precio": "3.500 $", "ubicacion": "Heladera"},
-    {"nombre": "Jugo de sabor multifruta (manzana, pera, banana y naranja) Pura Frutta 1 litro", "precio": "3.500 $", "ubicacion": "Heladera"},
+    {"nombre": "Chips de chocolate", "precio": "$1.800 por 100 gramos", "categoria": "Producto a un precio por kilo", "ubicacion": "Heladera"},
+    {"nombre": "Jugo de manzana roja Pura Frutta 1 litro", "precio": "$3.500", "ubicacion": "Heladera"},
+    {"nombre": "Jugo de sabor multifruta (manzana, pera, banana y naranja) Pura Frutta 1 litro", "precio": "$3.500", "ubicacion": "Heladera"},
 ]
 
+# Función de búsqueda
+def buscar_producto(nombre_producto):
+    resultados = [producto for producto in productos if nombre_producto.lower() in producto['nombre'].lower()]
+    return resultados
+
+# Interfaz de usuario con Streamlit
+st.title('Buscador de Productos')
+nombre_a_buscar = st.text_input('Buscar producto por nombre:')
+
+if nombre_a_buscar:
+    resultados = buscar_producto(nombre_a_buscar)
+    if resultados:
+        st.write(f"Productos encontrados ({len(resultados)}):")
+        for producto in resultados:
+            st.write(f"Nombre: {producto['nombre']}, Precio: {producto['precio']}, Ubicación: {producto['ubicacion']}")
+    else:
+        st.write("No se encontraron productos con ese nombre.")
