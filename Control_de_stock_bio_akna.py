91bc26e8683bf07ead02c54c094920e4c1c28238
Martín Badino <151761027+MartoBadi@users.noreply.github.com>
2024-10-14 16:06:44 -0300

    Le agregué elementos al diccionario

--- Control_de_stock_bio_akna.py
+++ Control_de_stock_bio_akna.py
@@ -2,9 +2,11 @@ import streamlit as st
 
 # Definición de productos como diccionarios
 productos = [
-    {"nombre": "Chips de chocolate", "precio": "$1.800 por 100 gramos", "categoria": "Producto a un precio por kilo", "ubicacion": "Heladera"},
-    {"nombre": "Jugo de manzana roja Pura Frutta 1 litro", "precio": "$3.500", "ubicacion": "Heladera"},
-    {"nombre": "Jugo de sabor multifruta (manzana, pera, banana y naranja) Pura Frutta 1 litro", "precio": "$3.500", "ubicacion": "Heladera"},
+    {"nombre": "Chips de chocolate", "precio": "$1.800 por 100 gramos", "categoria": "Producto a un precio por kilo", "ubicacion": "Heladera", "stock en venta": 1, "stock en despensa: 0},
+    {"nombre": "Jugo de manzana roja Pura Frutta 1 litro", "precio": "$3.500", "ubicacion": "Heladera", "stock en venta": 2, "stock en despensa: 0},
+    {"nombre": "Jugo de sabor multifruta (manzana, pera, banana y naranja) Pura Frutta 1 litro", "precio": "$3.500", "ubicacion": "Heladera", "stock en venta": 2, "stock en despensa": 0},
+{"nombre": "Jugo de manzana y frutilla Pura Frutta 1 litro"  ,"precio": "$3.700" "  ,"ubicacion": "Heladera", "stock en venta": 2, "stock en despensa" 0    }
+{"nombre": "Kombucha Bravia sabor superberry (con frutos rojos) 473ml"  ,"precio": "$3.100"  ,"ubicacion":  "Heladera", "stock en venta": 2, "stock en despensa": 0  }
 ]
 
 # Función de búsqueda
